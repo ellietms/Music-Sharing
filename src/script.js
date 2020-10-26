0b123d7d9ebe21275b7a5760ee3007eed13a2fd2
ellie <ellie.tms1371@gmail.com>
2020-10-26 19:57:38 +0000

    add js codes

--- src/script.js
+++ src/script.js
@@ -2,3 +2,10 @@ import Amplify, {Storage} from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig)
+
+document.getElementById('upload-form').addEventListener('submit',event => {
+    event.preventDefault();
+    const file = document.getElementById('file-upload').files[0];
+    // put our file in storage, use the file's name as its S3 Key
+    Storage
+})
